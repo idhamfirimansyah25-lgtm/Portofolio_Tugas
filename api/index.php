5b512dd674bcf6f366e991431bf8dd59f7a39a01
idham <idhamfirimansyah25@gmail.com>
2026-06-03 16:28:51 +0700

    chore: configure vercel deployment for laravel 12 and vite

--- api/index.php
+++ api/index.php
@@ -1,17 +1,2 @@
 <?php
-
-// 1. Register The Auto Loader
-require __DIR__ . '/../vendor/autoload.php';
-
-// 2. Run The Application
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
